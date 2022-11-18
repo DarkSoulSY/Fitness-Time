fba4f5dc9d65f52e44e1accdfff8d92304e95d37
DarkSoulSY <nasser_abdullah1999@hotmail.com>
2022-11-18 18:07:52 +0300

    Added a navigation bar and modified fragments 'added Diary Fragment'

--- app/src/main/java/com/example/fitnesstime/ui/main/home/Main.kt
+++ app/src/main/java/com/example/fitnesstime/ui/main/home/Main.kt
@@ -2,8 +2,10 @@ package com.example.fitnesstime.ui.main.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.fitnesstime.R
 import com.example.fitnesstime.databinding.ActivityMainBinding
+import com.example.fitnesstime.ui.main.fragments.*
 
 class Main : AppCompatActivity() {
 
@@ -13,5 +15,31 @@ class Main : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.progress -> replaceFragment(ProgressFragment())
+                R.id.diary -> replaceFragment(DiaryFragment())
+                R.id.bmi -> replaceFragment(BodyMassIndexFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+            }
+            true
+        }
+
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment).commit()
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/fitnesstime/ui/signin/SignIn.kt
+++ app/src/main/java/com/example/fitnesstime/ui/signin/SignIn.kt
@@ -1,9 +1,11 @@
 package com.example.fitnesstime.ui.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitnesstime.R
 import com.example.fitnesstime.databinding.ActivitySignInBinding
+import com.example.fitnesstime.ui.main.home.Main
 
 class SignIn : AppCompatActivity() {
 
@@ -12,6 +14,11 @@ class SignIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignin.setOnClickListener {
+            intent = Intent(this, Main()::class.java)
+            startActivity(intent)
+        }
 
 
     }

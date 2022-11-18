fca7856aa7569880fa1aa56c56dc701351316564
DarkSoulSY <nasser_abdullah1999@hotmail.com>
2022-11-18 17:28:21 +0300

    Added Fragments

--- app/src/main/java/com/example/fitnesstime/ui/main/home/Main.kt
+++ app/src/main/java/com/example/fitnesstime/ui/main/home/Main.kt
@@ -3,14 +3,14 @@ package com.example.fitnesstime.ui.main.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitnesstime.R
-import com.example.fitnesstime.databinding.ActivityHomeBinding
+import com.example.fitnesstime.databinding.ActivityMainBinding
 
-class Home : AppCompatActivity() {
+class Main : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
